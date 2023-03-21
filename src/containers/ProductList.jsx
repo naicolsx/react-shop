b34add1ebffb50dc04cd32c17191786d301bdd9b
Natalia Fernandez <naicolsx@gmail.com>
2023-03-21 10:18:40 -0500

    Carrito de compras

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import ProductItem from "../components/ProductItem";
+import React from "react";
+import ProductItem from "@components/ProductItem";
 import useGetProducts from "../hooks/useGetProducts";
 import "@styles/ProductList.scss";
 
